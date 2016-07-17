0a01bfcd654fd5983756f20e08e2d7b8e26a6a23
C0deZLee <c0dezlee@gmail.com>
2016-07-17 10:38:48 -0400

    fix conflict and remove print

--- CourseSpider/spiders/course_spider.py
+++ CourseSpider/spiders/course_spider.py
@@ -14,10 +14,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 class CourseSpider(BaseSpider):
     name = "course_spider"
     start_urls = ['https://www.example.com']
-<<<<<<< HEAD
     existed_list = []
-=======
->>>>>>> origin/master
 
     def __init__(self):
         chromedriver = "/Users/SteveLeeLX/CodeRepository/CourseSpider/chromedriver"
@@ -29,7 +26,6 @@ class CourseSpider(BaseSpider):
             for line in existed:
                 d = json.loads(line)
                 self.existed_list.append(d['number'])
-            print self.existed_list
 
         driver = self.driver
         driver.get('https://public.lionpath.psu.edu/psc/CSPRD_2/EMPLOYEE/HRMS/c/PE_TE031.CLASS_SEARCH.GBL')
@@ -56,10 +52,6 @@ class CourseSpider(BaseSpider):
             t_end = time.time() + 5
             while (time.time() < t_end and driver.find_elements_by_xpath('//*[@id="MTG_CLASS_NBR$0"]') == []):
                 time.sleep(0.2)
-<<<<<<< HEAD
-=======
-                pass
->>>>>>> origin/master
             time.sleep(0.1)
             # iterate class
 
@@ -76,19 +68,12 @@ class CourseSpider(BaseSpider):
                 room = hxs.xpath('//*[@id="MTG_ROOM$'+ str(course_count) +'"]')
                 ins = hxs.xpath('//*[@id="MTG_INSTR$'+ str(course_count) +'"]')
 
-                print 'here is nbr'
-                print nbr
                 # iterate classes
                 while nbr != []:
-                    print 'at least im here?'
                     # generate class
                     # get basic info
                     item['number'] = nbr.css('::text').extract()[0]
-<<<<<<< HEAD
-                    print 'here is the item number ' + item['number']
-                    if item['number'] in self.existed_list:
-                        print 'the ' + item['number'] + 'is in list ' + self.existed_list
-                    else:
+                    if item['number'] not in self.existed_list:
                         item['time'] = daytime.css('::text').extract()[0]
                         item['room'] = room.css('::text').extract()[0]
                         item['instructor1'] = ins.css('::text').extract()[0]
@@ -124,47 +109,6 @@ class CourseSpider(BaseSpider):
                             # Return to basic info page
                             driver.find_element_by_xpath('//*[@id="CLASS_SRCH_WRK2_SSR_PB_BACK"]').click()
                     print 'after gen_class'
-=======
-                    item['time'] = daytime.css('::text').extract()[0]
-                    item['room'] = room.css('::text').extract()[0]
-                    item['instructor1'] = ins.css('::text').extract()[0]
-                    try:
-                        item['instructor2'] = ins.css('::text').extract()[1]
-                    except IndexError:
-                        pass
-
-                    if (item['room'] == 'APPT' or item['room'] == 'TBA') and item['time'] == 'TBA':
-                        pass
-                    else:
-                        # get the detail info
-                        driver.find_element_by_xpath('//*[@id="MTG_CLASSNAME$'+ str(course_count) +'"]').click()
-                        while (driver.find_elements_by_xpath('//*[@id="SSR_CLS_DTL_WRK_SSR_DESCRSHORT"]') == []):
-                            time.sleep(0.2)
-                            pass
-
-                        detail_hxc = Selector(text=driver.page_source)
-
-                        item['status'] = detail_hxc.xpath('//*[@id="SSR_CLS_DTL_WRK_SSR_DESCRSHORT"]').css('::text').extract()[0]
-                        item['fullName'] = detail_hxc.xpath('//*[@id="DERIVED_CLSRCH_DESCR200"]').css('::text').extract()[0]
-                        item['unit'] = detail_hxc.xpath('//*[@id="SSR_CLS_DTL_WRK_UNITS_RANGE"]').css('::text').extract()[0]
-                        item['description'] = detail_hxc.xpath('//*[@id="DERIVED_CLSRCH_DESCRLONG"]').css('::text').extract()[0]
-                        item['capacity'] = detail_hxc.xpath('//*[@id="SSR_CLS_DTL_WRK_ENRL_CAP"]').css('::text').extract()[0]
-                        item['waitlist'] = detail_hxc.xpath('//*[@id="SSR_CLS_DTL_WRK_WAIT_CAP"]').css('::text').extract()[0]
-                        item['enrolled'] = detail_hxc.xpath('//*[@id="SSR_CLS_DTL_WRK_ENRL_TOT"]').css('::text').extract()[0]
-                        item['waitlistEnrolled'] = detail_hxc.xpath('//*[@id="SSR_CLS_DTL_WRK_WAIT_TOT"]').css('::text').extract()[0]
-
-                        try:
-                            item['classType'] = detail_hxc.xpath('//*[@id="SSR_CLS_DTL_WRK_SSR_CRSE_ATTR_LONG"]').css('::text').extract()[0]
-                            item['notes'] = detail_hxc.xpath('//*[@id="DERIVED_CLSRCH_SSR_CLASSNOTE_LONG"]').css('::text').extract()[0]
-                        except:
-                            pass
-
-                        yield item
-                        # Return to basic info page
-                        driver.find_element_by_xpath('//*[@id="CLASS_SRCH_WRK2_SSR_PB_BACK"]').click()
-
-
->>>>>>> origin/master
                     # increase
                     course_count = course_count + 1
                     # wait for load
@@ -182,14 +126,6 @@ class CourseSpider(BaseSpider):
 
                 # back to main page
                 driver.find_element_by_xpath('//*[@id="CLASS_SRCH_WRK2_SSR_PB_MODIFY$5$"]').click()
-<<<<<<< HEAD
-=======
-                major_count = major_count + 1
-                while(driver.find_elements_by_xpath("//*[@id=\"SSR_CLSRCH_WRK_SUBJECT_SRCH$2\"]") == []):
-                    time.sleep(0.2)
-                    pass
->>>>>>> origin/master
-
 
             major_count = major_count + 1
             # wait load
